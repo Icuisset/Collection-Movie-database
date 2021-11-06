8b4e7b85ed460b487dc9ea6b35733450f065d46a
Isabelle Cuisset <71969004+Icuisset@users.noreply.github.com>
2021-11-06 11:00:50 +0000

    detail card works well

--- src/components/elements/MovieCardDetail/MovieCardDetail.tsx
+++ src/components/elements/MovieCardDetail/MovieCardDetail.tsx
@@ -14,13 +14,28 @@ function MovieCardDetail(): JSX.Element {
 
     let { id } = useParams<{id: string}>();
 
-    const [selectedMovie, setSelectedMovie] = useState("tt0076759");
+    const [selectedMovie, setSelectedMovie] = useState({
+      title:"Star Wars",
+      year:"2022",
+      type:"Adventure",
+      poster:"https://images.unsplash.com/photo-1533613220915-609f661a6fe1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=928&q=8",
+      plot:"An amazing story from the stars",
+      rating:"8.5"
+    });
 
     useEffect(() => {
       moviesApi
         .getResultbyId(id)
         .then((result) => {
           console.log(result);
+          setSelectedMovie({
+            title:result.Title,
+            year:result.Year,
+            type:result.Genre,
+            poster:result.Poster,
+            plot:result.Plot,
+            rating:result.imdbRating
+          });
         })
         .catch((err) => {
           console.log(err);
@@ -28,32 +43,19 @@ function MovieCardDetail(): JSX.Element {
     }
   , [id]);
    
-    interface Card { 
-        Title : string;
-        Year : string;
-        Type : string;
-        Poster : string;
-        imdbID : string;}
-
-
-    const selectedTitle = "No movie selected";
-    const selectedType = "No movie selected";
-    const selectedPlot="There is no plot yet";
-    const selectedPoster="https://images.unsplash.com/photo-1533613220915-609f661a6fe1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=928&q=80";
-    const selectedRating ="8.5";
-
     return (
       <section className="movieCardDetailSection">
           <div className="movieCardDetail">
           <div className="movieCardDetail__image-box">
-          <img className="movieCardDetail__image" src={selectedPoster} alt="movie poster"/>
+          <img className="movieCardDetail__image" src={selectedMovie.poster} alt="movie poster"/>
           </div>
           <div className="movieCardDetail__right">
-          <h2 className="movieCardDetail__title">{selectedTitle}</h2>
+          <h2 className="movieCardDetail__title">{selectedMovie.title}</h2>
           <div className="movieCardDetail__title_underlined"></div>
-          <p className="movieCardDetail__text">{selectedPlot}</p>
-          <p className="movieCardDetail__type">{selectedType}</p>
-          <p className="movieCardDetail__rating">{selectedRating}</p>
+          <p className="movieCard__year">{selectedMovie.year}</p>
+          <p className="movieCardDetail__text">{selectedMovie.plot}</p>
+          <p className="movieCardDetail__type">{selectedMovie.type}</p>
+          <p className="movieCardDetail__rating">rating: {selectedMovie.rating}</p>
           <Link className="movieCardDetail__link" to='/'><button className='goback__button' type='button'>Go Back</button></Link>
           </div>
           </div>
