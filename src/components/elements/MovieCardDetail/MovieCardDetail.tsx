d7c1c053fe2d2d5db5ee84ec69bfa2242390e202
Isabelle Cuisset <71969004+Icuisset@users.noreply.github.com>
2021-11-07 07:52:52 +0000

    added loading spinner

--- src/components/elements/MovieCardDetail/MovieCardDetail.tsx
+++ src/components/elements/MovieCardDetail/MovieCardDetail.tsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 
 import './MovieCardDetail.css';
 import moviesApi from '../../../utils/moviesApi';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 
  
 
@@ -14,19 +15,33 @@ function MovieCardDetail(): JSX.Element {
     let { id } = useParams<{id: string}>();
 
     const [selectedMovie, setSelectedMovie] = useState({
-      title:"Sorry, there is no film with this ID",
+      title:"",
       year:"",
       type:"",
-      poster:"https://images.unsplash.com/photo-1533613220915-609f661a6fe1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=928&q=8",
-      plot:"Please search for another film",
+      poster:"",
+      plot:"",
       rating:""
     });
 
+   const [isLoading, setIsLoading] = useState(false);
+
     useEffect(() => {
+      setIsLoading(true);
       moviesApi
         .getResultbyId(id)
         .then((result) => {
           console.log(result);
+          if (result.Response === "False") {
+            setSelectedMovie({
+              title:"Sorry, there is no film with this ID",
+              year:"",
+              type:"",
+              poster:"https://images.unsplash.com/photo-1533613220915-609f661a6fe1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=928&q=8",
+              plot:"Please search for another film",
+              rating:""
+            });
+            setIsLoading(false);
+          };
           if (result.Response === "True") {
           setSelectedMovie({
             title:result.Title,
@@ -35,7 +50,9 @@ function MovieCardDetail(): JSX.Element {
             poster:result.Poster,
             plot:result.Plot,
             rating:result.imdbRating
-          });}
+          });
+          setIsLoading(false);
+        }
         })
         .catch((err) => {
           console.log(err);
@@ -45,21 +62,23 @@ function MovieCardDetail(): JSX.Element {
    
     return (
       <section className="movieCardDetailSection">
-          <div className="movieCardDetail">
-          <div className="movieCardDetail__image-box">
-          <img className="movieCardDetail__image" src={selectedMovie.poster} alt="movie poster"/>
-          </div>
-          <div className="movieCardDetail__right">
-          <h2 className="movieCardDetail__title">{selectedMovie.title}</h2>
-          <div className="movieCardDetail__title_underlined"></div>
-          <p className="movieCard__year">{selectedMovie.year}</p>
-          <p className="movieCardDetail__text">{selectedMovie.plot}</p>
-          <p className="movieCardDetail__type">{selectedMovie.type}</p>
-          { selectedMovie.rating?
-          (<p className="movieCardDetail__rating">rating: {selectedMovie.rating}</p>): null}
-          <Link className="movieCardDetail__link" to='/'><button className='goback__button' type='button'>Go Back</button></Link>
-          </div>
-          </div>
+        {isLoading ?
+        (<LoadingSpinner/>):
+        (<div className="movieCardDetail">
+        <div className="movieCardDetail__image-box">
+        <img className="movieCardDetail__image" src={selectedMovie.poster} alt="movie poster"/>
+        </div>
+        <div className="movieCardDetail__right">
+        <h2 className="movieCardDetail__title">{selectedMovie.title}</h2>
+        <div className="movieCardDetail__title_underlined"></div>
+        <p className="movieCard__year">{selectedMovie.year}</p>
+        <p className="movieCardDetail__text">{selectedMovie.plot}</p>
+        <p className="movieCardDetail__type">{selectedMovie.type}</p>
+        { selectedMovie.rating?
+        (<p className="movieCardDetail__rating">rating: {selectedMovie.rating}</p>): null}
+        <Link className="movieCardDetail__link" to='/'><button className='goback__button' type='button'>Go Back</button></Link>
+        </div>
+        </div>)}
           </section>
     )
   };
